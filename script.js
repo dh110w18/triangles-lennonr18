9a2f08dc1423948feb04a2335e141f63343d23e0
Ryan Lennon <Ryan@tests-MacBook-Pro.local>
2018-03-08 16:42:22 -0500

    update

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ document.getElementById("first").innerHTML = message;
 // Part 2: Hypotenuse of a triangle //
 
 function calcHypotenuse (b, h) {
-  console.log("")
-}
+  console.log(Math.sqrt(b*b + h*h))
+};
 
 // Part 3: Triangle Object //
 
@@ -22,9 +22,9 @@ var triangle = {
   height: 40,
   canvasId: "canvas",
   calcArea: function () {
-   console.log (this.base * this.height)/2;
+   console.log ((this.base * this.height)/2);
 }
-}
+};
 
 var triangle = {
   base: 30,
@@ -34,12 +34,21 @@ var triangle = {
     console.log (Math.sqrt(this.base*this.base + this.height*this.height))
 }
 };
-  
+
+var triangle = {
+  base: 30,
+  height: 40,
+  canvasId: "canvas",
+  drawIt: function () {
+    drawTriangle(this.base, this.height, this.canvasId)
+}
+};
 
 // Part 4: User entered base and height //
 
 var userBase = prompt ("Please enter a value","for the triangle's base","that is greater than 10");
-var userHeight = prompt ("Please enter a value","for the triangle's height","that is greater than 0")
+var userHeight = prompt ("Please enter a value","for the triangle's height","that is greater than 0");
+
 if (userBase <=10) {
   console.log ("Error!");
 } else if (userHeight <=1) {
@@ -47,7 +56,20 @@ if (userBase <=10) {
 } else {
   ("userBase = this.base","userHeight = this.height")
 };
+
 var areaMessage = ("The area of the triangle is " + ((base *height)/2));
 var hypotMessage = ("The hypotenuse of the triangle is" + (Math.sqrt(this.base*this.base + this.height*this.height)));
 
+document.getElementById("canvas").innerHTML = message;
+
 // Par 5: Lots of triangles //
+
+var i = 1;
+
+while (i++ <= 5) {
+  base*i;
+  height*i;
+}
+window.drawIt
+canvasId = canvas2;
+triangle.drawIt ();
